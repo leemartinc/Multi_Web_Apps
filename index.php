7128eae1c0694e96a47fbd159a3941f4e1a390c9
Lee-Martin Clarke <contact@leemartinclarke.com>
2021-05-23 13:56:17 -0400

    html rename

--- index.php
+++ index.php
@@ -1,1 +1,86 @@
-<?php include 'index.html'; ?> 
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Hello World</title>
+    <!-- JavaScript INCOMING -->
+    <script type="text/javascript" src=""></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+
+    <!-- CSS INCOMING -->
+    <link href="css/buttons.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="css/hamburg.css" rel="stylesheet" type="text/css" />
+    <link href="css/assn2.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="main">
+
+
+
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <ul style="color:white; list-style-type: square; font-size:25px;">
+                <li><a href="assignment_2/submitassn2login.html">Assignment 2</a></li>
+                <li><a href="assignment_3/assn3-main.html">Assignment 3</a></li>
+                <li><a href="assignment_4/assn4.html">Assignment 4</a></li>
+                <li><a href="assignment_5/assn5-main.html">Assignment 5</a></li>
+                <li><a href="assignment_6/assn6-main.html">Assignment 6</a></li>
+                <li><a href="assignment_7/assn7.xml">Assignment 7</a></li>
+                <li><a href="project_1/proj1.html">Project 1</a></li>
+                <li><a href="assignment_8/assn8-main.html">Assignment 8</a></li>
+                <li><a href="project_2/e_library/login.php">Project 2</a></li>
+                <li><a href="assignment_9/assn9-main.php">Assignment 9</a></li>
+                <li><a href="assignment_10/assn5main.html">Assignment 10</a></li>
+            </ul>
+        </div>
+
+        <div class="assn animated bounceInRight delay-02s">
+            <span class="fl-rt" style="font-size:30px;cursor:pointer;color: aliceblue;" onclick="openNav()">&#9776; Assignments</span> </div>
+
+        <script>
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "260px";
+            }
+
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
+
+        </script>
+
+
+        <!--Background
+        <video class="main-bg" autoplay loop muted>
+                <source src="vid/motion1.mp4" id="myVideo" type="video/mp4" />
+                Your browser does not support HTML5 video.
+        </video>-->
+
+
+        <div class="midcontainer">
+            <h2 class="center animated fadeInDown delay-05s" style="margin:20px;">CSC 4730 Web Programming</h2>
+
+            <p class="center animated fadeInDown delay-05s">Lee-Martin Clarke</p><br/>
+            <!--<p>&nbsp;</p>-->
+
+            <div class="center">
+                <a class="button2 animated fadeInDown delay-05s" style="text-decoration: none;" href="http://leemartinc.com">About Me</a><br />
+
+                <a class="button2 animated fadeInDown delay-05s" href="http://validator.w3.org/check?uri=referer">Validate XHTML</a>
+                <p>
+                    <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
+                </p>
+            </div>
+        </div>
+
+        <div class="center center-x" style="bottom:0;">
+            <img class="animated zoomIn delay-10s" src="img/spin.gif" height="150px;" alt="page-down-hint" />
+        </div>
+    </div>
+
+</body>
+
+</html>
